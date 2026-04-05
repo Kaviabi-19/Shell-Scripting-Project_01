b590f670a8be9289f3ab69efb63960e6104545ca
Kavi_Devops <kavipriyabai388@gmail.com>
2026-04-05 11:04:16 +0530

    Update Script.sh

--- Script.sh
+++ Script.sh
@@ -3,7 +3,7 @@
 
 ###############################################################################
 # Author: Kavipriyabai
-# Version: v0.0.1
+# Version: v0.0.2
 
 # Script to automate the process of listing all the resources in an AWS account
 #
